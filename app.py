c6153c5796168b0c1fadb18b24e9d74c2746fbdf
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-16 21:36:46 -0400

    Addressing cors issues

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import requests
+from flask_cors import CORS
 
 # creating the flask app
 app = Flask(__name__)
+# Allow CORS for all domains on all routes (for development purposes)
+CORS(app)
 
 @app.route('/v1/api/ai/respond', methods=['POST'])
 def respond():
